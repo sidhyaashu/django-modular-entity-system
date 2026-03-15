37029df3aab4e5d6dd25c3dc9fcc636fafdef656
sidhyaashu <ashutoshsidhya69@gmail.com>
2026-03-15 11:48:09 +0530

    Project setup done

---
+++ modular_entity_system/modular_entity_system/urls.py
@@ -0,0 +1,50 @@
+"""
+URL configuration for modular_entity_system project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.contrib import admin
+from django.urls import path, include
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Modular Entity API",
+      default_version='v1',
+      description="Vendor Product Course Certification API",
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    
+        path('api/', include('vendor.urls')),
+    path('api/', include('product.urls')),
+    path('api/', include('course.urls')),
+    path('api/', include('certification.urls')),
+
+    path('api/', include('vendor_product_mapping.urls')),
+    path('api/', include('product_course_mapping.urls')),
+    path('api/', include('course_certification_mapping.urls')),
+
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0)),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0)),
+]
